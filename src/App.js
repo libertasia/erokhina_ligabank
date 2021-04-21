709326454293374382ef1289d7179a48996c2af3
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-04-21 20:10:36 +0300

    Markup WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/logo.svg'
+import './scss/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      {/* <header className="header">
+        <nav className="main-navigation">
+          <a className="main-header-logo" href="info.html">
+            <img src={logo} width={28} height={25} alt="Логотип Лига Банк»" />
+            <p>Лига Банк</p>
+          </a>
+          <ul className="site-navigation">
+            <li><a href="info.html">Услуги</a></li>
+            <li><a href="loan.html">Рассчитать кредит</a></li>
+            <li><a href="converter.html">Конвертер валют</a></li>
+            <li><a href="contacts.html">Контакты</a></li>
+            <li><a href="question.html">Задать вопрос</a></li>
+          </ul>
+          <ul className="user-navigation">
+            <li><a className="login-link" href="login.html">Войти в Интернет-банк</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <main>
+        
+      </main>
+
+      <footer>
+
+      </footer> */}
     </div>
   );
 }
